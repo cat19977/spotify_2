b4d2a978b5f7603b0ed4fb9b32080061a94b6b4c
Cat Squillante <catsquillante@cats-air.myfiosgateway.com>
2021-01-28 15:56:41 -0500

    new

--- src/Configs/settings.js
+++ src/Configs/settings.js
@@ -1,3 +1,4 @@
-export const clientId = 'a6164587c1e3458a99f433290cf8d250';
-export const redirectUri = 'http://localhost:3000';
+export const clientId = 'd9dca95c2ee54a54b35c890972268f0a';
+//export const redirectUri = 'http://localhost:3000';
+export const redirectUri = 'https://cat19977.github.io/spotify_2/'
 export const scope = 'user-top-read user-library-read user-read-recently-played'
